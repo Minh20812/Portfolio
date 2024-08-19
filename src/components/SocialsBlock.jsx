b91eb72e33123a19e4cd428e6f3ecbf561a0faf7
Minh20812 <blossworld12@gmail.com>
2024-08-19 22:07:43 +0700

    Initial

---
+++ src/components/SocialsBlock.jsx
@@ -0,0 +1,65 @@
+import Block from "./Block";
+import { SiGithub, SiTiktok, SiTwitter, SiYoutube } from "react-icons/si";
+
+const SocialsBlock = () => (
+  <>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-red-500 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiYoutube />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-green-600 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiGithub />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "-2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-zinc-50 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-black"
+      >
+        <SiTiktok />
+      </a>
+    </Block>
+    <Block
+      whileHover={{
+        rotate: "2.5deg",
+        scale: 1.1,
+      }}
+      className="col-span-6 bg-blue-500 md:col-span-3"
+    >
+      <a
+        href="#"
+        className="grid h-full place-content-center text-3xl text-white"
+      >
+        <SiTwitter />
+      </a>
+    </Block>
+  </>
+);
+
+export default SocialsBlock;
